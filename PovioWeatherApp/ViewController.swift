05614ba6afd572f123c6c855c25c45f29b9ab04f
Viktor Tapovski <viktor.tapovski@gmail.com>
2016-02-22 13:32:22 +0100

    Initial Commit

---
+++ PovioWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PovioWeatherApp
+//
+//  Created by Viktor Tapovski on 22.2.16.
+//  Copyright © 2016 Viktor Tapovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
